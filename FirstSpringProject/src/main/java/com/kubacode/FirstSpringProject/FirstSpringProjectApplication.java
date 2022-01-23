200fdcc261b64d5fe4cca3fa019c82311e9c0f24
KubaRed <49131826+KubaRed@users.noreply.github.com>
2022-01-23 23:50:48 +0100

    Add files via upload

---
+++ FirstSpringProject/src/main/java/com/kubacode/FirstSpringProject/FirstSpringProjectApplication.java
@@ -0,0 +1,21 @@
+package com.kubacode.FirstSpringProject;
+
+import com.kubacode.FirstSpringProject.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class FirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProjectApplication.class, args);
+	}
+
+
+}
